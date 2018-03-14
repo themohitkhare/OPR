b9bf203e9414b763505a92c61930b564898467cc
mohitkhare582 <mohitkhare582@gmail.com>
2018-03-14 21:08:11 +0530

    Contains methods to return either alternative products or similar products based of the reviews of the selected product.

---
+++ Recomender.py
@@ -0,0 +1,21 @@
+'''
+Module contains methods that will produce the list of recommendations based on the reviews of users.
+'''
+
+
+def alternate_product(product_ID):
+    '''
+    A function that gives the list of alternative products due to either negative reviews or absence of reviews.
+
+    :param product_ID:
+    :return:
+    '''
+
+
+def accompany_product(product_ID):
+    '''
+    Method that returns the list of products that can be bought with selected product because of the positive reviews of
+     the users.
+    :param product_ID:
+    :return:
+    '''
